221adfa7bbc9612043dfe3fd23339b1feb49cb07
Daniel Saukel <sa.da@unitybox.de>
2018-09-21 02:02:47 +0200

    Fix WG7 integration

--- src/com/dre/brewery/integration/WGBarrel7.java
+++ src/com/dre/brewery/integration/WGBarrel7.java
@@ -4,13 +4,16 @@ package com.dre.brewery.integration;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import com.dre.brewery.P;
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldedit.extension.platform.Actor;
 import com.sk89q.worldedit.util.Location;
 import com.sk89q.worldedit.world.World;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
-import com.sk89q.worldguard.bukkit.permission.RegionPermissionModel;
+import com.sk89q.worldguard.internal.permission.RegionPermissionModel;
 import com.sk89q.worldguard.internal.platform.WorldGuardPlatform;
 import com.sk89q.worldguard.protection.flags.Flags;
 import com.sk89q.worldguard.protection.regions.RegionQuery;
@@ -23,7 +26,8 @@ public class WGBarrel7 implements WGBarrel {
 
                 World world = platform.getWorldByName(spigot.getWorld().getName());
 		if (!platform.getGlobalStateManager().get(world).useRegions) return true; // Region support disabled
-		if (new RegionPermissionModel(wg, player).mayIgnoreRegionProtection(spigot.getWorld())) return true; // Whitelisted cause
+		WorldEditPlugin we = JavaPlugin.getPlugin(WorldEditPlugin.class);
+		if (new RegionPermissionModel((Actor) we.wrapPlayer(player)).mayIgnoreRegionProtection(world)) return true; // Whitelisted cause
 
 		RegionQuery query = platform.getRegionContainer().createQuery();
 

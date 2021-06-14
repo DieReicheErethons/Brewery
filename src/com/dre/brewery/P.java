d4af10426a93f1b9267a73ea348481e2b967dd17
Sn0wStorm <milan-alb@hotmail.de>
2021-06-15 01:40:12 +0200

    Added License

--- src/com/dre/brewery/P.java
+++ src/com/dre/brewery/P.java
@@ -1,3 +1,27 @@
+/**
+ *
+ *     Brewery Minecraft-Plugin for an alternate Brewing Process
+ *     Copyright (C) 2021 Milan Albrecht
+ *
+ *     This file is part of Brewery.
+ *
+ *     Brewery is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Brewery is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Brewery.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+
 package com.dre.brewery;
 
 import com.dre.brewery.filedata.BConfig;

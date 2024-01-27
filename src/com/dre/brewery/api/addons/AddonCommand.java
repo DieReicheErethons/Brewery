48055a0e1fa4d96ffe8364a8f3b88e2677770f44
jonah <jonahmarksinclair@gmail.com>
2024-01-27 17:30:23 -0500

    command support for addons

--- src/com/dre/brewery/api/addons/AddonCommand.java
+++ src/com/dre/brewery/api/addons/AddonCommand.java
@@ -6,7 +6,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
 public abstract class AddonCommand extends BukkitCommand {
-	protected AddonCommand(@NotNull String name, @NotNull String description, @NotNull String usageMessage, @NotNull List<String> aliases) {
+	public AddonCommand(@NotNull String name, @NotNull String description, @NotNull String usageMessage, @NotNull List<String> aliases) {
 		super(name, description, usageMessage, aliases);
 	}
 }
